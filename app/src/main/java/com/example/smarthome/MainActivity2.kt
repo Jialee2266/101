41be052b6c57aab689300b544a82f11e2164960f
jaden-letsgo <tl.li@hotmail.com>
2022-09-13 03:14:58 -0700

    Finalized MainActivity2, Fixed CameraX bug

--- app/src/main/java/com/example/smarthome/MainActivity2.kt
+++ app/src/main/java/com/example/smarthome/MainActivity2.kt
@@ -1,24 +1,19 @@
 package com.example.smarthome
 
-
-
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.os.Build
-import android.os.Bundle
-import android.os.Handler
-import android.os.StrictMode
-import android.os.StrictMode.ThreadPolicy
+import android.os.*
 import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.camera.core.*
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.video.*
-import androidx.camera.video.VideoCapture
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.PermissionChecker
@@ -30,114 +25,105 @@ import okhttp3.Request
 import okhttp3.RequestBody.Companion.asRequestBody
 import java.io.File
 import java.io.IOException
+import java.util.*
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
-
-//typealias LumaListener = (luma: Double) -> Unit
-
-
+//Screen 3: Practice Page Allow User to Record Gestures
 class MainActivity2 : AppCompatActivity() {
+
+    private val client = OkHttpClient()
     private lateinit var viewBinding: ActivityMain2Binding
-
-    private var imageCapture: ImageCapture? = null
-
     private var videoCapture: VideoCapture<Recorder>? = null
     private var recording: Recording? = null
-
     private lateinit var cameraExecutor: ExecutorService
-
-
-    val MEDIA_TYPE_MARKDOWN = "text/x-markdown; charset=utf-8".toMediaType()
     private var value = ""
+    private var name = ""
+    private var count = 0
+
+    //Page initial page3
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
         viewBinding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(viewBinding.root)
-        val policy = ThreadPolicy.Builder().permitAll().build()
+
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
         StrictMode.setThreadPolicy(policy)
 
-        // Request camera permissions
+        val value = intent.extras
+        if (value != null)
+        {
+            name = value!!.getString("key")!!
+        }
+
         if (allPermissionsGranted()) {
             startCamera()
         } else {
             ActivityCompat.requestPermissions(
                 this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
         }
-
-
-
-        // Set up the listeners for take photo and video capture buttons
-
-        viewBinding.imageButton.setOnClickListener { takePhoto() }
+        viewBinding.imageButton.setOnClickListener { end() }
         viewBinding.videoButton.setOnClickListener { captureVideo() }
         cameraExecutor = Executors.newSingleThreadExecutor()
-
     }
 
+    /*
+     *CameraX Code mainly done by following android studio documentation.
+     *https://developer.android.com/training/camerax
+     */
 
-
-
-
-
-    private fun takePhoto() {
+    //Done with recording
+    private fun end() {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
     }
-
-
-    // Implements VideoCapture use case, including start and stop capturing.
+    //Start recording
     private fun captureVideo() {
-
+        count += 1
+        //Ensure is not recording currently
         val curRecording = recording
         if (curRecording != null) {
-            // Stop the current recording session.
             curRecording.stop()
             recording = null
             return
         }
         val videoCapture = this.videoCapture ?: return
-
         viewBinding.videoButton.isEnabled = false
 
-
-        // create and start a new recording session
-        val name = value
+        //Build file name
         val contentValues = ContentValues().apply {
-            put(MediaStore.MediaColumns.DISPLAY_NAME, "h")
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
             put(MediaStore.MediaColumns.MIME_TYPE, "video/mp4")
             if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
                 put(MediaStore.Video.Media.RELATIVE_PATH, "Movies/CameraX-Video")
             }
         }
 
+        //Output path to local folder
         val mediaStoreOutputOptions = MediaStoreOutputOptions
             .Builder(contentResolver, MediaStore.Video.Media.EXTERNAL_CONTENT_URI)
             .setContentValues(contentValues)
             .build()
 
+        //Start recording
         recording = videoCapture.output
             .prepareRecording(this, mediaStoreOutputOptions)
             .apply {
                 if (PermissionChecker.checkSelfPermission(this@MainActivity2,
                         Manifest.permission.RECORD_AUDIO) ==
-                    PermissionChecker.PERMISSION_GRANTED)
-                {
-                    withAudioEnabled()
-                }
+                    PermissionChecker.PERMISSION_GRANTED) {}
             }
             .start(ContextCompat.getMainExecutor(this)) { recordEvent ->
                 when(recordEvent) {
                     is VideoRecordEvent.Start -> {
                         viewBinding.videoButton.apply {
-                            text = getString(R.string.stop_capture)
-                            isEnabled = true
+                            isEnabled = false
                         }
+                        Handler(Looper.getMainLooper()).postDelayed({
+                            recording?.stop()
+                        }, 5000)
                     }
-
-
                     is VideoRecordEvent.Finalize -> {
                         if (!recordEvent.hasError()) {
                             val msg = "Video capture succeeded: " +
@@ -145,6 +131,10 @@ class MainActivity2 : AppCompatActivity() {
                             Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT)
                                 .show()
                             Log.d(TAG, msg)
+
+                            //Send to Flask
+                            toFlask()
+                            recording = null
                         } else {
                             recording?.close()
                             recording = null
@@ -152,20 +142,21 @@ class MainActivity2 : AppCompatActivity() {
                                     "${recordEvent.error}")
                         }
                         viewBinding.videoButton.apply {
-                            text = getString(R.string.start_capture)
                             isEnabled = true
                         }
                     }
                 }
             }
-
     }
 
+    //Turn on camera and start preview
     private fun startCamera() {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
 
         cameraProviderFuture.addListener({
             // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
             val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
 
             // Preview
@@ -175,13 +166,12 @@ class MainActivity2 : AppCompatActivity() {
                     it.setSurfaceProvider(viewBinding.viewFinder.surfaceProvider)
                 }
 
-            // Select back camera as a default
-
+            //Config camera
             val recorder = Recorder.Builder()
                 .setQualitySelector(QualitySelector.from(Quality.HIGHEST))
                 .build()
             videoCapture = VideoCapture.withOutput(recorder)
-            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
             try {
                 // Unbind use cases before rebinding
                 cameraProvider.unbindAll()
@@ -189,16 +179,13 @@ class MainActivity2 : AppCompatActivity() {
                 // Bind use cases to camera
                 cameraProvider.bindToLifecycle(
                     this, cameraSelector, preview,videoCapture)
-
             } catch(exc: Exception) {
                 Log.e(TAG, "Use case binding failed", exc)
             }
-
         }, ContextCompat.getMainExecutor(this))
-
-
     }
 
+    //Check system permissions
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
         ContextCompat.checkSelfPermission(
             baseContext, it) == PackageManager.PERMISSION_GRANTED
@@ -210,7 +197,9 @@ class MainActivity2 : AppCompatActivity() {
 
     }
 
+    //Addition const
     companion object {
+        private val MEDIA_TYPE_MARKDOWN = "text/x-markdown; charset=utf-8".toMediaType()
         private const val TAG = "CameraXApp"
         private const val REQUEST_CODE_PERMISSIONS = 10
         private val REQUIRED_PERMISSIONS =
@@ -223,10 +212,12 @@ class MainActivity2 : AppCompatActivity() {
                 }
             }.toTypedArray()
     }
+
+    //Check permission before start camera
+    @SuppressLint("MissingSuperCall")
     override fun onRequestPermissionsResult(
         requestCode: Int, permissions: Array<String>, grantResults:
         IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == REQUEST_CODE_PERMISSIONS) {
             if (allPermissionsGranted()) {
                 startCamera()
@@ -237,6 +228,35 @@ class MainActivity2 : AppCompatActivity() {
                 finish()
             }
         }
+    }
+
+
+    /*Flask code mainly done by following OKHTTP3 documentation.
+     *https://square.github.io/okhttp/recipes/
+     */
+    private fun toFlask() {
+
+        //Build requestBody with video file
+        val file = File("/sdcard/Movies/CameraX-Video/" +name+ ".mp4")
+        val requestBody = MultipartBody.Builder()
+            .setType(MultipartBody.FORM)
+            .addFormDataPart(
+                "file", name.uppercase() + "_PRACTICE_" + count + ".mp4",
+                file.asRequestBody(MEDIA_TYPE_MARKDOWN))
+            .build()
+
+        //Build request to internet ip
+        val request = Request.Builder()
+            .url("http://192.168.0.25:5000/")
+            .post(requestBody)
+            .build()
+
+        client.newCall(request).execute().use { response ->
+            if (!response.isSuccessful) throw IOException("Unexpected code $response")
+            println(response.body!!.string())
+        }
+        //Delete recorded file from local folder after it is transferred to server
+        file.delete()
     }
 }
 
